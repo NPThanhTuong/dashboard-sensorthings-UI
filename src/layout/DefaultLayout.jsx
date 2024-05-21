f120b9b49ca24ee4cbdad009b1ef8df3b73c9faf
NguyenHThaiK2 <thainguyenk242@gmail.com>
2024-05-21 20:46:17 +0700

    edit file DefaultLayout: The LoginPage only appears when not logged in

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,21 +1,35 @@
-// import Navbar from "../components/navbar_component/Navbar";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/navbar_component/Navbar";
 import Sidebar from "../components/sidebar_component/Sidebar";
-
 import UserRoutes from "../routes/UserRoutes";
+import { useAuth } from "../context/AuthContext";
+import LoginPage from "../pages/login_page/LoginPage";
 
 const DefaultLayout = () => {
+  const { token } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/dang-nhap");
+    }
+  }, [token, navigate]);
+
+  if (!token) {
+    return <LoginPage />;
+  }
+
   return (
-    <>
-      <div className="flex">
-        <div className="h-dvh w-[10%] rounded-sm border bg-gradient-to-t from-cyan-400 to-blue-500 text-white">
-          <Sidebar />
-        </div>
-        <div className="w-[90%] p-2">
-          {/* <Navbar /> */}
-          <UserRoutes />
-        </div>
+    <div className="flex">
+      <div className="h-dvh w-[10%] rounded-sm border bg-white">
+        <Sidebar />
       </div>
-    </>
+      <div className="w-[90%] bg-gray-100 p-2">
+        <Navbar />
+        <UserRoutes />
+      </div>
+    </div>
   );
 };
 
