353c476cc5b8fb80f179b70cb38aa0e8d6a340b9
NguyenHThaiK2 <thainguyenk242@gmail.com>
2024-05-21 23:16:51 +0700

    create file Light: Display light data

--- src/components/control_component/Light.jsx
+++ src/components/control_component/Light.jsx
@@ -30,13 +30,21 @@ const Light = () => {
     };
 
     fetchLight();
-  }, []);
+
+    const interval = setInterval(() => {
+      fetchLight();
+    }, 3000); // Sau 3s sẽ fetch dữ liệu 1 lần
+
+    return () => clearInterval(interval); // đảm bảo interval sẽ được dọn dẹp khi component unmount, ngăn ngừa việc gọi hàm fetch khi không cần thiết
+  }, [token]);
 
   return (
-    <div className="flex w-full items-center justify-center border-4 border-yellow-300">
-      <div className="text-center text-xl font-bold">
-        <p className="">Ánh sáng</p>
-        <p className="">{light !== null ? `${light} (lux)` : "Đang tải..."}</p>
+    <div className="flex w-full items-center border-4 border-yellow-300 px-4 py-3">
+      <div className="text-xl">
+        <span className="">Ánh sáng: </span>
+        <span className="font-bold">
+          {light !== null ? `${light} (lux)` : "Đang tải..."}
+        </span>
       </div>
     </div>
   );
