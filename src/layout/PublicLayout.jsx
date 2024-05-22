03f34f65ddefd418d5de2d208051f5740284acd0
NguyenHThaiK2 <thainguyenk242@gmail.com>
2024-05-22 22:26:19 +0700

    edit file PublicLayout

--- src/layout/PublicLayout.jsx
+++ src/layout/PublicLayout.jsx
@@ -3,9 +3,7 @@ import { Outlet } from "react-router-dom";
 const PublicLayout = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md rounded bg-white p-6 shadow-md">
-        <Outlet />
-      </div>
+      <Outlet />
     </div>
   );
 };
